d4784c4aad40d18e4439286245e29a7199af3892
Aryan Raj Sinha <arajsinha.ars@gmail.com>
2023-05-06 18:50:09 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import Script from 'next/script';
+import Head from 'next/head';
 
 import { Toaster } from "react-hot-toast";
 import { Layout } from "../components";
@@ -8,12 +10,34 @@ import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={'https://www.googletagmanager.com/gtag/js?id=G-1XNG744J5J'}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-1XNG744J5J', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
+      <Head>
+        <title>Welcome!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <StateContext>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+    </>
   );
 }
 
